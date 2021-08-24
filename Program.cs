1415793c33043f052402522d578cca2f8a2c6bb2
Kyle Melton <KyleMeltonCodes@gmail.com>
2021-08-24 12:42:13 -0500

    Adds first and second number clarification and project requirements comments

--- Program.cs
+++ Program.cs
@@ -10,14 +10,15 @@ namespace Calculator
             {
                 int result = 0;
 
-                Console.Write("Enter a Number: ");
+                Console.Write("Enter first number: ");
                 int firstNum = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Enter a Number: ");
+                Console.Write("Enter a second number: ");
                 int secondNum = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("Choose an operator: \n Add: + \n Multiply: x \n Divide: / \nEnter: ");
                 string userChoice = Console.ReadLine();
+
 
                 switch (userChoice)
                 {
@@ -48,3 +49,13 @@ namespace Calculator
         }
     }
 }
+
+/*
+   PROJECT OBJECTIVES
+    Console App [x]
+    Manage via Git/Hub [x]
+    Demonstrate branch [x]
+    Min of 6 commits [x]
+    Follow conventions to best of my ability [x]
+    Validate uer input via catch [x]
+*/
