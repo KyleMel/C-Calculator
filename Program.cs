2d5661db128665a3311e785ca9aaa4a42b09fa27
Kyle Melton <KyleMeltonCodes@gmail.com>
2021-08-24 12:27:52 -0500

    "Math Update"
    Adds int variable for storing result and string for what operator the
    user selects. Create Switch/Case statements for doing the math.

--- Program.cs
+++ Program.cs
@@ -7,18 +7,39 @@ namespace Calculator
         static void Main(string[] args)
         {
             try
-            { 
-            Console.Write("Enter a Number: ");
-            int firstNum = Convert.ToInt32(Console.ReadLine());
+            {
+                int result = 0;
 
-            Console.Write("Enter a Number: ");
-            int secondNum = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter a Number: ");
+                int firstNum = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Choose an operator: \n Add: + \n Multiply: x \n Divide: / \nEnter: ");
-            Console.ReadLine();
+                Console.Write("Enter a Number: ");
+                int secondNum = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Results: \n Press Enter to exit");
-            Console.ReadLine();
+                Console.Write("Choose an operator: \n Add: + \n Multiply: x \n Divide: / \nEnter: ");
+                string userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "+":
+                        result = firstNum + secondNum;
+                        break;
+                    case "-":
+                        result = firstNum - secondNum;
+                        break;
+                    case "x":
+                        result = firstNum * secondNum;
+                        break;
+                    case "/":
+                        result = firstNum - secondNum;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                        
+                }
+                Console.Write($"Results: {result}\nPress Enter to exit");
+                Console.ReadLine();
             }
             catch (Exception e)
             {
